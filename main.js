171b6202f3fbfe2771d0f2e91cfc2079d712a279
Michal Szemryk <szemrykmichal@gmail.com>
2023-05-20 12:13:49 +0200

    build weather api

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const apiKey =  '&appid=a2d49474a738a688862e11c9d8974976';
 const apiUnits = '&units=metric';
 const apiLang = '&lang=pl';
+
+getWeather = () =>{
+    const apiCity = input.value;
+    const URL = apiLink + apiCity + apiKey + apiUnits + apiLang;
+
+    axios.get(URL).then(response => {
+        console.log(response.data);
+        cityName.textContent = `${response.data.name}, ${response.data.sys.country}`;
+        img.src= `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+        temperature.textContent = `${response.data.main.temp} C`;
+        weatherDesc.textContent = `${response.data.weather[0].description}`
+        feelsLike.textContent = `${response.data.main.feels_like} C`;
+        windSpeed.textContent = `${response.data.wind.speed} km/h`;
+        clouds.textContent = `${response.data.clouds.all} %`;
+        humidity.textContent = `${response.data.main.humidity} %`;
+        press.textContent = `${response.data.main.pressure} hPa`;
+    }).catch().finally();
+   
+}
+
+button.addEventListener('click',getWeather);
